25ae6369505072b3ce472c50453d6a4eea451cde
Toshiyuki Oshima <toshiyuki67026@gmail.com>
2022-09-03 21:04:21 +0900

    update

--- Controller.m
+++ Controller.m
@@ -1,48 +1,53 @@
 classdef Controller
 
     properties (Access = private)
-        % pRPLIDAR = CreateRPLIDAR();
-        config_file_path = 'config.txt'
+        pRPLIDAR = CreateRPLIDAR();
+        config_file_path = 'config.txt';
     end
 
     methods
 
         function obj = Controller(~)
             addpath('Hardware-MATLAB')
-            disp('Constructor')
-            % hardwarex_init;
         end
 
         function obj = SwitchConnection(obj, value)
 
             if strcmp(value, 'On')
-                obj = obj.Connect()
+                obj = obj.Connect();
             elseif strcmp(value, 'Off')
-                obj.Disconnect()
+                obj.Disconnect();
             end
 
         end
 
         function obj = Connect(obj)
             disp('Connect')
-            % obj.pRPLIDAR = CreateRPLIDAR();
-            % [result] = ConnectRPLIDAR(obj.pRPLIDAR, obj.config_file_path)
+            obj.pRPLIDAR = CreateRPLIDAR();
+            [result] = ConnectRPLIDAR(obj.pRPLIDAR, obj.config_file_path);
         end
 
         function Disconnect(obj)
             disp('Disconnect')
-            % [result] = DisconnectRPLIDAR(obj.pRPLIDAR)
-            % DestroyRPLIDAR(obj.pRPLIDAR);
+            [result] = DisconnectRPLIDAR(obj.pRPLIDAR);
+            DestroyRPLIDAR(obj.pRPLIDAR);
         end
 
-        function ScanAndPlot(obj)
-            disp('ScanAndPlot')
-            % [result, distance, angle, bNewScan, quality] = GetScanDataResponseRPLIDAR(obj.pRPLIDAR);
+        function [x, y] = Scan(obj)
+            disp('Scan')
+            x = [];
+            y = [];
+
+            for i = 1:10
+                [result, distance, angle, bNewScan, quality] = GetScanDataResponseRPLIDAR(obj.pRPLIDAR);
+                x = [x, distance * cos(angle)];
+                y = [y, distance * sin(angle)];
+            end
+
         end
 
         function Destructor(~)
-            disp('Destructor')
-            % unloadlibrary('hardwarex');
+            unloadlibrary('hardwarex');
         end
 
     end
